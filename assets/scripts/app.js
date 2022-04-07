bd977fa63093bbec594ecfa40913f79751b1d031
Mamata Adhikari <adh.mamata@gmail.com>
2022-04-07 20:00:46 +1000

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,57 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+//gets inputs from user
+function getUserNumberInput(){
+return parseInt(userInput.value);
+// `${sfafs} and ${ssd}`
+}
+
+//generate outputs/result.
+function creatAndWriteOutput(operator, resultBeforeCal, calcNumber){
+        const calDiscription = `${resultBeforeCal} ${operator} ${calcNumber}`;
+        outputResult (currentResult, calDiscription);
+
+}
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult
+    currentResult = currentResult + enteredNumber;
+    creatAndWriteOutput('+',initialResult,enteredNumber);
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    creatAndWriteOutput('-', initialResult, enteredNumber);
+
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    creatAndWriteOutput('*', initialResult, enteredNumber);
+
+
+}
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    creatAndWriteOutput('/', initialResult, enteredNumber);
+
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click',divide)
+
+
+
+
+
+
